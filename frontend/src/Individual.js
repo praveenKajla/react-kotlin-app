4c9203d04c95adc20f2520eb232eb49c6b0877bd
praveenKajla <praveenkajla187@gmail.com>
2017-08-23 18:47:20 +0530

    update

--- frontend/src/Individual.js
+++ frontend/src/Individual.js
@@ -22,10 +22,11 @@ class Individual extends Component{
       similar:[],
       isLoading:true,
       isDelete:false,
+      isUpdate:false,
 
     };
     this.delete = this.delete.bind(this);
-
+    this.handleChange = this.handleChange.bind(this);
   }
 
   delete(){
@@ -37,15 +38,40 @@ class Individual extends Component{
 
   }
 
+  handleChange(event){
+    const {details} = this.state;
+    this.setState({details:{...details,[event.target.name]:event.target.value}});
+    event.preventDefault()
+  }
 
   componentWillMount(){
      const {params} = this.props.match;
-     axios.get(`${PATH}${params.type}/${params.typeid}?${apiKey}`)
-      .then(result => {this.setState({details:result.data,isLoading:false});});
+
+     axios.get(`http://localhost:8090/api/get/${params.type}/praveen/${params.typeid}`)
+      .then(result => {console.log(result.data);this.setState({details:result.data,isLoading:false});});
       
     axios.get(`${PATH}${params.type}/${params.typeid}/similar?${apiKey}`)
       .then(result => {this.setState({similar:result.data.results});});
   }
+
+  updateChanges(){
+    this.setState({isUpdate:false,isLoading:true})
+    const {type} = this.props.match.params;
+    const item = this.state.details;
+  if(type=="movie"){
+    item.video = {}
+   axios.post('http://localhost:8090/api/movie/praveen',
+              item).then(response => 
+                 this.setState({isLoading:false})                          
+              )
+  }
+  else{
+   axios.post('http://localhost:8090/api/tv/praveen',
+              item).then(response => this.setState({isLoading:false}) )
+    }
+  }
+
+  
   
   render(){
     const {details,isLoading} = this.state;
@@ -69,7 +95,7 @@ class Individual extends Component{
             <div >
             <button href="/home"className="buttonDelete" onClick={() => this.setState({isDelete:true})}>
             Delete </button>
-            <button href="/home"className="buttonDelete" >
+            <button href="/home"className="buttonDelete" onClick={() => this.setState({isUpdate:true})}>
             Update </button>
             </div>
             <div className="mylist">
@@ -89,21 +115,63 @@ class Individual extends Component{
                     position          : 'absolute',
                     backgroundColor   : 'rgba(0, 0, 0, 0.3)'
                   }}}
+                contentLabel="delete"
            isOpen={this.state.isDelete} >
            <a className="dropText">Are You Sure You Want to Delete this ?</a>
            <button className="buttonF" onClick={() => this.delete()}>Yes</button>
            <button className="buttonXX" onClick={() => this.setState({isDelete:false})}>X</button>
           </ReactModal> 
 
-          <ReactModal  className="animated bounceInDown modal"
+          <ReactModal  className="animated bounceInDown modalUpdate"
            style={{ overlay : {
                     position          : 'absolute',
                     backgroundColor   : 'rgba(0, 0, 0, 0.3)'
                   }}}
-           isOpen={this.state.isDelete} >
-           <a className="dropText">Are You Sure You Want to Delete this ?</a>
-           <button className="buttonF" onClick={() => this.delete()}>Yes</button>
-           <button className="buttonXX" onClick={() => this.setState({isDelete:false})}>X</button>
+            contentLabel="update"      
+           isOpen={this.state.isUpdate} >
+
+
+          <div className="short">
+           {(params.type=="movie")
+          ?
+          <div>
+            <p className="overviewText">Title</p>
+                    <input className="inpTitle"type="text" value={details.original_title} name="original_title" onChange={this.handleChange} />
+          </div>
+          :
+          <div>
+            <p className="shortText">Title</p>
+                    <input className="inpTitle"type="text" value={details.original_name} name="original_name" onChange={this.handleChange} />
+          </div>
+          }
+           
+         </div>
+
+           <p className="overviewText">Overview</p>
+          <textarea className="inpOverview"type="text" value={details.overview} name="overview" onChange={this.handleChange} />
+
+          <div className="short" >
+          {(params.type=="movie")
+          ?
+          <div>
+            <p className="shortText">Release Date</p>
+                    <input className="inpRelease"type="text" value={details.release_date} name="release_date" onChange={this.handleChange} />
+          </div>
+          :
+          <div>
+            <p className="shortText">First AirDate</p>
+                    <input className="inpRelease"type="text" value={details.first_air_date} name="first_air_date" onChange={this.handleChange} />
+          </div>
+          }
+           <p className="shortText">Vote Average</p>
+          <input className="inpRelease"type="text" value={details.vote_average} name="vote_average" onChange={this.handleChange} />
+          <p className="shortText">Popularity</p>
+          <input className="inpRelease"type="text" value={details.popularity} name="popularity" onChange={this.handleChange} />
+          </div>
+
+
+          <button className="buttonF" onClick={() => this.updateChanges()}>Update</button>
+          <button className="buttonXX" onClick={() => this.setState({isUpdate:false})}>X</button>
           </ReactModal>    
             
           </div>
